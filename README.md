241e50a73ee3373f9c08e41e2b4274527d1643cb
Rudi Daemen <fludizz@gmail.com>
2013-06-27 23:24:51 +0200

    Following changes: * Made the code more resistent to sensor failures * Serial output is now JSON * Display output is neater on sensor failiures
    Tested with 20m UTP for the remote probes.

--- README.md
+++ README.md
@@ -9,5 +9,16 @@ Components
 ==========
 * Arduino
 * 2x DS18B20
+* 1x 4.7k Ohm resistor (Pull-up OneWire bus)
 * 2x DHT11
+* 2x 10k Ohm resistor (Pull-up DHT Data bus)
 * 16x4 HD44780 compatible display
+* 1.8k Ohm resistor (Contrast for LCD)
+* 2x RJ45 socket
+
+DHT11 Library
+=============
+
+This project uses the generic DHT11 library, however for the JSON output to work
+on the serial line, the DHT.cpp file must be editted to remove the lines 
+containing "Serial.print()"
--- WeatherDuino.ino
+++ WeatherDuino.ino
@@ -4,13 +4,15 @@
 #include <DallasTemperature.h>
 
 // DHT sensor settings
-// Connect pin 1 (on the left) of the sensor to +5V
-// Connect pin 2 of the sensor to whatever Datapin you choose
-// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect DHT pin 1 (on the left) of the sensor to +5V
+// Connect DHT pin 2 of the sensor to whatever Datapin you choose
+// Connect DHT pin 4 (on the right) of the sensor to GROUND
 // Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+// Note: Edit the DHT.cpp file, remove the Serial.print() lines. Those prints
+//       mess up the JSON output from the WeatherDuino
 DHT dht1(3, DHT11); // #1 is connected to Pin3 and type is DHT11
 DHT dht2(4, DHT11); // #2 is connected to Pin4 and type is DHT11
-
+  
 // LCD Panel Connections:
 // rs (LCD pin 4) to Arduino pin 12
 // rw (LCD pin 5) to Arduino pin 11
@@ -19,7 +21,8 @@ DHT dht2(4, DHT11); // #2 is connected to Pin4 and type is DHT11
 LiquidCrystal lcd(12, 11, 10, 9, 8, 7, 6);
 
 // Data wire is plugged into port 2 on the Arduino
-// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+// Setup a oneWire instance to communicate with any OneWire devices (not just 
+// Maxim/Dallas temperature ICs)
 OneWire oneWire(2);
 
 // Pass our oneWire reference to Dallas Temperature. 
@@ -30,8 +33,6 @@ DeviceAddress Probe02 = { 0x28, 0x35, 0xD8, 0xC6, 0x04, 0x00, 0x00, 0xD6 };
 
 void setup() {
   Serial.begin(57600); 
-  Serial.println("[WeatherDuino]");
-
   lcd.begin(16,4);              // columns, rows.  use 16,2 for a 16x2 LCD, etc.
   lcd.clear();                  // start with a blank screen
   lcd.setCursor(0,0);           // set cursor to column 0, row 0 (the first row)
@@ -42,17 +43,16 @@ void setup() {
   lcd.print("by Fludizz");
   lcd.setCursor(-4,3);
   lcd.print("DHT11 & DS18B20");
-  delay(1000);
+  delay(1000);                  // Show bootscreen for 1 second!
+  
+  // Start preparing the initial screen!
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print("Probe01:");
-  lcd.setCursor(0,1);
-  lcd.print("");
   lcd.setCursor(-4,2);
   lcd.print("Probe02:");
-  lcd.setCursor(-4,3);
-  lcd.print("");
   
+  // Start all the probes!
   dht1.begin();
   dht2.begin();
   onewire.begin();
@@ -66,41 +66,70 @@ void loop() {
   float hout = dht2.readHumidity();
   float tin = onewire.getTempC(Probe01);
   float tout = onewire.getTempC(Probe02);
-
-  // check if returns are valid, if they are NaN (not a number) then something went wrong!
-  Serial.print("H-in: "); 
-  Serial.print(hin);
-  Serial.print("\tT-in: "); 
+  
+  // Check if returns are valid, if they are NaN (not a number) then something went wrong!
+  // Note: DHT11 sometimes returns 0 instead of NaN if it fails to read the probe.
+  Serial.print("{ \"WeatherDuino\": { \"Probe1\": { \"Temp\": "); 
   Serial.print(tin);
-  Serial.print("\tH-out: "); 
-  Serial.print(hout);
-  Serial.print("\tT-out: "); 
-  Serial.println(tout);
-  lcd.setCursor(1,1);
-  if ( tin < 10 && tin > 0 ) {
-    lcd.print(" ");
+  Serial.print(", \"Humid\": ");
+  if ( isnan(hin) || hin == 0 ) {
+    Serial.print(-127.00);
+  } else {
+    Serial.print(hin);
   }
-  lcd.print(tin, 1);
+  Serial.print(" }, \"Probe2\": { \"Temp\": ");
+  Serial.print(tout);
+  Serial.print(", \"Humid\": ");
+  if ( isnan(hout) || hout == 0 ) {
+    Serial.print(-127.00);
+  } else {
+    Serial.print(hout);
+  }
+  Serial.println(" } } }");
+
+  // LCD line 2
+  lcd.setCursor(1,1);
+  if ( tin < -99 || tin > 99 ) {
+    lcd.print("--.-");
+  } else if ( tin < 10 && tin > 0 ) {
+    lcd.print(" ");
+    lcd.print(tin, 1);
+  } else {
+    lcd.print(tin, 1);
+  }
   lcd.print(char(223));
   lcd.print("C ");
   lcd.setCursor(9,1);
-  if ( hin < 10 ) {
+  if ( isnan(hin) || hin == 0 ) {
+    lcd.print("--");
+  } else if ( hin < 10 ) {
     lcd.print(" ");
+    lcd.print(hin, 0);
+  } else {
+    lcd.print(hin, 0);
   }
-  lcd.print(hin, 0);
   lcd.print("% ");
-  lcd.setCursor(-2,3);
+
+  // LCD Line 4
   lcd.setCursor(-3,3);
-  if ( tout < 10 && tout > 0 ) {
+  if ( tout < -99 || tout > 99 ) {
+    lcd.print("--.-");
+  } else if ( tout < 10 && tout > 0 ) {
     lcd.print(" ");
+    lcd.print(tout, 1);
+  } else {
+    lcd.print(tout, 1);
   }
-  lcd.print(tout, 1);
   lcd.print(char(223));
   lcd.print("C ");
   lcd.setCursor(5,3);
-  if ( hout < 10 ) {
+  if ( isnan(hout) || hout == 0 ) {
+    lcd.print("--");
+  } else if ( hout < 10 ) {
     lcd.print(" ");
+    lcd.print(hout, 0);
+  } else {
+    lcd.print(hout, 0);
   }
-  lcd.print(hout, 0);
   lcd.print("% ");
 }
