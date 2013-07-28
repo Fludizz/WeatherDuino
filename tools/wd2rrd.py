d5f26d1811a10c87a9893a3ee0a1511716b1ac26
Rudi Daemen <fludizz@gmail.com>
2013-07-28 18:10:53 +0200

    Fixed the parser, it now actually works and logs data to configurable paths/files.

--- tools/wd2rrd.py
+++ tools/wd2rrd.py
@@ -14,6 +14,7 @@ import sys
 import optparse
 import os
 import serial
+import time
 try:
     import json
 except ImportError:
@@ -46,7 +47,7 @@ TIMEDELTA = {"day": "-86400",
              "year": "-31536000" }
 
 
-def ProcessRRDdata(path, rrd, name, unit, inval, outval, defs=DEFRRD):
+def ProcessRRDdata(path, rrd, name, axis_unit, inval, outval, defs=DEFRRD):
   ''' This function checks (and optionally creates) the required rrd file and
   will update this file with the data provided. After updating the file, it will
   generate the required graphs. '''
@@ -56,6 +57,11 @@ def ProcessRRDdata(path, rrd, name, unit, inval, outval, defs=DEFRRD):
   else:
     rrdfile = rrd
     imgname = name
+  # Dirty fix for the escaping mismatch... RRDtool uses the % sign in *some*
+  # parameters as the escaping character but not in the axis unit. This cause
+  # the escape value to break the rrd graph generation. If the unit is %, make
+  # it an escaped % sign ('%%') and don't do this for the axis.
+  unit = axis_unit.replace('%', '%%')
   # Check if the rrdfile exists and create if needed.
   if not os.path.isfile(rrdfile):
     rrdtool.create(rrdfile, defs)
@@ -64,74 +70,83 @@ def ProcessRRDdata(path, rrd, name, unit, inval, outval, defs=DEFRRD):
   # Generate graphs for each configured Delta in TIMEDELTA:
   for delta in TIMEDELTA.keys():
     if delta == "day":
-      rrdtool.graph("%s-%s.png" % (imgname, delta), 
-                    "--start", TIMEDELTA[delta],
-                    "--vertical-label=%s (%s)" % (name, unit),
-                    "--slope-mode",
-                    "DEF:indoor=%s:In:AVERAGE" % rrdfile,
-                    "DEF:outdoor=%s:Out:AVERAGE" % rrdfile,
-                    "LINE1:indoor#00FF00:Indoor\:",
-                    "GPRINT:indoor:AVERAGE:%6.1lf" + unit,
-                    "LINE1:outdoor#0000FF:Outdoor\:",
-                    "GPRINT:outdoor:AVERAGE:%6.1lf" + unit + "\\r")
+      rrdtool.graph(
+          "%s-%s.png" % (imgname, delta), 
+          "--start", TIMEDELTA[delta],
+          "--vertical-label=%s (%s)" % (name, axis_unit),
+          "--slope-mode",
+          "DEF:indoor=%s:In:AVERAGE" % rrdfile,
+          "DEF:outdoor=%s:Out:AVERAGE" % rrdfile,
+          "LINE1:indoor#00FF00:Indoor\:",
+          "GPRINT:indoor:AVERAGE:%%6.1lf%s" % unit,
+          "LINE1:outdoor#0000FF:Outdoor\:",
+          "GPRINT:outdoor:AVERAGE:%%6.1lf%s\\r" % unit)
     else:
-      rrdtool.graph("%s-%s.png" % (imgname, delta), 
-                "--start", TIMEDELTA[delta],
-                "--vertical-label=%s (%s)" % (name, unit),
-                "--slope-mode",
-                "DEF:inavg=%s:In:AVERAGE" % rrdfile,
-                "DEF:outavg=%s:Out:AVERAGE" % rrdfile,
-                "DEF:inmin=%s:In:MIN" % rrdfile,
-                "DEF:outmin=%s:Out:MIN" % rrdfile,
-                "DEF:inmax=%s:In:MAX" % rrdfile,
-                "DEF:outmax=%s:Out:MAX" % rrdfile,
-                "LINE1:inmax#FFFF00:In Max:",
-                "GPRINT:inmax:MAX:%6.1lf" + unit,
-                "LINE1:outmax#00FFFF:Out Max:",
-                "GPRINT:outmax:MAX:%6.1lf" + unit + "\\r",
-                "LINE1:inavg#00FF00:In Avg:",
-                "GPRINT:inavg:AVERAGE:%6.1lf" + unit,
-                "LINE1:outavg#0000FF:Out Avg:",
-                "GPRINT:outavg:AVERAGE:%6.1lf" + unit + "\\r",
-                "LINE1:inmin#AAFF00:In Min:",
-                "GPRINT:inmin:MIN:%6.1lf" + unit,
-                "LINE1:outmin#00AAFF:Out Min:",
-                "GPRINT:outmin:MIN:%6.1lf" + unit + "\\r")
+      rrdtool.graph(
+          "%s-%s.png" % (imgname, delta), 
+          "--start", TIMEDELTA[delta],
+          "--vertical-label=%s (%s)" % (name, axis_unit),
+          "--slope-mode",
+          "DEF:inavg=%s:In:AVERAGE" % rrdfile,
+          "DEF:outavg=%s:Out:AVERAGE" % rrdfile,
+          "DEF:inmin=%s:In:MIN" % rrdfile,
+          "DEF:outmin=%s:Out:MIN" % rrdfile,
+          "DEF:inmax=%s:In:MAX" % rrdfile,
+          "DEF:outmax=%s:Out:MAX" % rrdfile,
+          "LINE1:inmax#FFFF00:In Max:",
+          "GPRINT:inmax:MAX:%%6.1lf%s" % unit,
+          "LINE1:outmax#00FFFF:Out Max:",
+          "GPRINT:outmax:MAX:%%6.1lf%s\\r" % unit,
+          "LINE1:inavg#00FF00:In Avg:",
+          "GPRINT:inavg:AVERAGE:%%6.1lf%s" % unit,
+          "LINE1:outavg#0000FF:Out Avg:",
+          "GPRINT:outavg:AVERAGE:%%6.1lf%s\\r" % unit,
+          "LINE1:inmin#AAFF00:In Min:",
+          "GPRINT:inmin:MIN:%%6.1lf%s" % unit,
+          "LINE1:outmin#00AAFF:Out Min:",
+          "GPRINT:outmin:MIN:%%6.1lf%s\\r" % unit)
 
 
 def GetWeatherDevice(device="/dev/ttyUSB0", baud=57600):
   ''' Generate a device/object useable for the program, if it is a valid device
   (e.g. a WeatherDuino). '''
   weatherduino = serial.Serial(device, baud, timeout=10)
+  weatherduino.setDTR(True)
+  weatherduino.setDTR(False)
   # Clear the initial junk from the buffer
+  weatherduino.flushInput()
   weatherduino.readline()
   if "WeatherDuino" in json.loads(weatherduino.readline()):
+    print "Found WeatherDuino device on %s at %s." % (device,baud)
     return weatherduino
   else:
     raise Exception('Device is not a WeatherDuino!')
 
 
-def ContinualRRDwrite(arduino, destination):
+def ContinualRRDwrite(arduino, fdestination, fprefix):
   ''' With the returned WeatherDuino device, run an endless loop collecting and
   writing weather data. '''
-  if not os.path.exists(destination):
+  if not os.path.exists(fdestination):
     raise Exception('Destination path does not exist.')
+  print "Writing weatherdata to files '%s%s*'" % (fdestination,fprefix)
   while True:
     arduino.flushInput()
-    weatherduino.readline()
-    data = json.loads(arduino.readline().strip())['WeatherDuino']
-    for items in data:
+    arduino.readline()
+    data = arduino.readline().strip()
+    for items in json.loads(data)['WeatherDuino']:
       if items['probe'] == 1:
         intemp, inhum = items['temp'], items['humid']
       elif items['probe'] == 2:
         outtemp, outhum = items['temp'], items['humid']
       else:
         print "Ignoring probe with ID %i" % items['probe']
-    ProcessRRDdata(destination, "WeatherDuino_temp.rrd", "Temp", "°C",
-                   intemp, outtemp)
-    ProcessRRDdata(destination, "WeatherDuino_humid.rrd", "Humid", "%",
-                   inhum, outhum)
-    time.sleep(300)
+    print "[%s] Probe 1: T%s H%s; Probe 2: T%s H%s" % (time.ctime(), intemp, 
+                                                       inhum, outtemp, outhum)
+    ProcessRRDdata(fdestination, "%s_temp.rrd" % fprefix, "Temp", 
+                   u"\u00B0C".encode('utf8'), intemp, outtemp)
+    ProcessRRDdata(fdestination, "%s_humid.rrd" % fprefix, "Humid", 
+                   u"%".encode('utf8'), inhum, outhum)
+    time.sleep(60)
   
   
 if __name__ == '__main__':
@@ -141,8 +156,10 @@ if __name__ == '__main__':
                     help="The serial device the Arduino is connected to.")
   parser.add_option("-b", "--baud", metavar="BAUD", default=57600, type="int",
                     help="Baudrate at which the Arduino is communicating.")
-  parser.add_option("-p", "--path", metavar="PATH", default=None,
+  parser.add_option("-p", "--path", metavar="PATH", default="./",
                     help="Destination path where the RRD & graphs are written")
+  parser.add_option("-f", "--file", metavar="FILE", default="WeatherDuino",
+                    help="Filename prefix for the RRD files.")
   (opts, args) = parser.parse_args()
   try:
     print "%s: Unrecognized argument \'%s\'" % (sys.argv[0], args[0])
@@ -150,7 +167,5 @@ if __name__ == '__main__':
     sys.exit(1)
   except IndexError:
     pass
-  print "Writing data from device %s at %i baud to '%s'..." % (opts.device, 
-                                                               opts.baud,
-                                                               opts.path)
-  ContinualRRDwrite(GetWeatherDevice(opts.device, opts.baud), opts.path)
+  weatherduino = GetWeatherDevice(opts.device, opts.baud)
+  ContinualRRDwrite(weatherduino, opts.path, opts.file)
